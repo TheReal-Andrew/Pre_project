d171093b45b8d664c4bca63f0a9977236b6eb9a0
TheReal-Andrew <lindahlhtx@gmail.com>
2022-11-23 10:39:28 +0100

    Mappe

---
+++ Pre-project/Case 2/case2_MAA.py
@@ -0,0 +1,310 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 10 12:41:30 2022
+
+@author: lukas
+"""
+
+should_solve = True
+Should_MGA   = False
+Should_MAA   = False
+n_snapshots  = 2000
+n_objective  = 16780122009.380968
+mga_slack    = 0.0001
+
+#%% Import
+import pypsa 
+import numpy as np
+from pypsa.linopt import get_var, linexpr, join_exprs, define_constraints, get_dual, get_con, write_objective, get_sol, define_variables
+from pypsa.descriptors import nominal_attrs
+from pypsa.linopf import lookup, network_lopf, ilopf
+from pypsa.pf import get_switchable_as_dense as get_as_dense
+from pypsa.descriptors import get_extendable_i, get_non_extendable_i
+import matplotlib.pyplot as plt
+import pandas as pd
+from scipy.spatial import ConvexHull
+import os
+import sys
+
+#%% MGA functions
+def extra_functionality(n,snapshots,options,direction):
+    define_mga_constraint(n,snapshots,options['mga_slack'])
+    define_mga_objective(n,snapshots,direction,options)
+
+
+
+def assign_carriers(n):
+    """
+    Author: Fabian Neumann 
+    Source: https://github.com/PyPSA/pypsa-eur-mga
+    """
+
+    if "Load" in n.carriers.index:
+        n.carriers = n.carriers.drop("Load")
+
+    if "carrier" not in n.lines:
+        n.lines["carrier"] = "AC"
+
+    if n.links.empty:
+        n.links["carrier"] = pd.Series(dtype=str)
+
+    config = {
+        "AC": {"color": "rosybrown", "nice_name": "HVAC Line"},
+        "DC": {"color": "darkseagreen", "nice_name": "HVDC Link"},
+    }
+    for c in ["AC", "DC"]:
+        if c in n.carriers.index:
+            continue
+        n.carriers = n.carriers.append(pd.Series(config[c], name=c))
+
+def define_mga_constraint(n, sns, epsilon=None, with_fix=False):
+    """
+    Author: Fabian Neumann 
+    Source: https://github.com/PyPSA/pypsa-eur-mga
+    
+    Build constraint defining near-optimal feasible space
+    Parameters
+    ----------
+    n : pypsa.Network
+    sns : Series|list-like
+        snapshots
+    epsilon : float, optional
+        Allowed added cost compared to least-cost solution, by default None
+    with_fix : bool, optional
+        Calculation of allowed cost penalty should include cost of non-extendable components, by default None
+    """
+
+    if epsilon is None:
+        epsilon = float(snakemake.wildcards.epsilon)
+
+    if with_fix is None:
+        with_fix = snakemake.config.get("include_non_extendable", True)
+
+    expr = []
+
+    # operation
+    for c, attr in lookup.query("marginal_cost").index:
+        cost = (
+            get_as_dense(n, c, "marginal_cost", sns)
+            .loc[:, lambda ds: (ds != 0).all()]
+            .mul(n.snapshot_weightings.loc[sns,'objective'], axis=0)
+        )
+        if cost.empty:
+            continue
+        expr.append(linexpr((cost, get_var(n, c, attr).loc[sns, cost.columns])).stack())
+
+    # investment
+    for c, attr in nominal_attrs.items():
+        cost = n.df(c)["capital_cost"][get_extendable_i(n, c)]
+        if cost.empty:
+            continue
+        expr.append(linexpr((cost, get_var(n, c, attr)[cost.index])))
+
+    lhs = pd.concat(expr).sum()
+
+    if with_fix:
+        ext_const = objective_constant(n, ext=True, nonext=False)
+        nonext_const = objective_constant(n, ext=False, nonext=True)
+        rhs = (1 + epsilon) * (n.objective_optimum + ext_const + nonext_const) - nonext_const
+    else:
+        ext_const = objective_constant(n)
+        rhs = (1 + epsilon) * (n.objective_optimum + ext_const)
+
+    define_constraints(n, lhs, "<=", rhs, "GlobalConstraint", "mu_epsilon")
+
+def objective_constant(n, ext=True, nonext=True):
+    """
+    Author: Fabian Neumann 
+    Source: https://github.com/PyPSA/pypsa-eur-mga
+    """
+
+    if not (ext or nonext):
+        return 0.0
+
+    constant = 0.0
+    for c, attr in nominal_attrs.items():
+        i = pd.Index([])
+        if ext:
+            i = i.append(get_extendable_i(n, c))
+        if nonext:
+            i = i.append(get_non_extendable_i(n, c))
+        constant += n.df(c)[attr][i] @ n.df(c).capital_cost[i]
+
+    return constant
+
+
+def define_mga_objective(n,snapshots,direction,options):
+    mga_variables = options['mga_variables']
+    expr_list = []
+    for dir_i,var_i in zip(direction,mga_variables):
+        model_vars = get_var(n,var_i[0],'p_nom')[n.df(var_i[0]).carrier == var_i[1]]
+        tmp_expr = linexpr((dir_i/len(model_vars),model_vars)).sum()
+        expr_list.append(tmp_expr)
+
+    mga_obj = join_exprs(np.array(expr_list))
+    write_objective(n,mga_obj)
+
+
+def get_var_values(n,mga_variables):
+
+    variable_values = {}
+    for var_i in variables:
+        val = n.df(variables[var_i][0]).query('carrier == "{}"'.format(variables[var_i][1])).p_nom_opt.sum()
+        variable_values[var_i] = val
+
+    return variable_values
+
+#%% Load and solve network
+
+n = pypsa.Network('connected_buses3.nc') #Load network from netcdf file
+
+# Reduce snapshots used for faster computing
+n.snapshots = n.snapshots[:n_snapshots]
+n.snapshot_weightings = n.snapshot_weightings[:n_snapshots] 
+
+# Change carrier for optimization
+n.links.loc[["Island_to_Denmark","Island_to_Germany"], "carrier"] = ["AC_DK", "AC_DE"]
+
+n_optimum = n.copy()
+
+if should_solve:
+    n_optimum.lopf(pyomo = False,
+          solver_name = 'gurobi'
+          )
+    
+    print("Network objective value:" + str(n_optimum.objective))
+    
+    n_objective = n_optimum.objective
+else:
+    pass
+
+# n_optimum = n.copy()
+
+#%% Carriers
+
+
+#%% MGA - Variables
+
+n.objective_optimum = n_objective
+
+variables = {'x1':('Link','AC_DK'),
+             'x2':('Link','AC_DE'),
+             'x3':('Generator','Wind'),
+            }
+
+#%% MGA - Search 1 direction
+
+if Should_MGA or Should_MAA:
+    direction = [1,1] # 1 means minimize, -1 means maximize 
+    mga_variables = ['x1','x2'] # The variables that we are investigating
+    
+    options = dict(mga_slack=mga_slack,
+                    mga_variables=[variables[v] for v in mga_variables])
+    
+    res = n.lopf(pyomo=False,
+            solver_name='gurobi',
+            keep_references=True,
+            keep_shadowprices=True,
+            skip_objective=True,
+            solver_options={'LogToConsole':0,
+                    'crossover':0,
+                    'BarConvTol' : 1e-6,                 
+                    'FeasibilityTol' : 1e-2,
+                'threads':2},
+            extra_functionality=lambda n,s: extra_functionality(n,s,options,direction)
+        )
+    
+    all_variable_values = get_var_values(n,mga_variables)
+    print(all_variable_values)
+else:
+    pass
+
+#%% MAA Functions
+def search_direction(direction,mga_variables):
+    options = dict(mga_slack=mga_slack,
+                    mga_variables=[variables[v] for v in mga_variables])
+
+    res = n.lopf(pyomo=False,
+            solver_name='gurobi',
+            #keep_references=True,
+            #keep_shadowprices=True,
+            skip_objective=True,
+            solver_options={'LogToConsole':0,
+                    'crossover':0,
+                    'BarConvTol' : 1e-6,                 
+                    'FeasibilityTol' : 1e-2,
+                'threads':2},
+            extra_functionality=lambda n,s: extra_functionality(n,s,options,direction)
+        )
+
+    all_variable_values = get_var_values(n,mga_variables)
+
+    return [all_variable_values[v] for v in mga_variables]
+
+#%% MAA - Run
+
+if Should_MAA:
+    MAA_convergence_tol = 0.05 # How much the volume stops changing before we stop, in %
+    dim=len(mga_variables) # number of dimensions 
+    dim_fullD = len(variables)
+    old_volume = 0 
+    epsilon = 1
+    directions_searched = np.empty([0,dim])
+    hull = None
+    computations = 0
+    
+    solutions = np.empty(shape=[0,dim])
+    
+    while epsilon>MAA_convergence_tol:
+    
+        if len(solutions) <= 1:
+            directions = np.concatenate([np.diag(np.ones(dim)),-np.diag(np.ones(dim))],axis=0)
+        else :
+            directions = -np.array(hull.equations)[:,0:-1]
+        directions_searched = np.concatenate([directions_searched,directions],axis=0)
+    
+        # Run computations in series
+        for direction_i in directions:
+            res = search_direction(direction_i,mga_variables)
+            solutions = np.append(solutions,np.array([res]),axis=0)
+    
+    
+        hull = ConvexHull(solutions)
+    
+        delta_v = hull.volume - old_volume
+        old_volume = hull.volume
+        epsilon = delta_v/hull.volume
+        print('####### EPSILON ###############')
+        print(epsilon)
+else:
+    pass
+
+#%% Plot Hull
+
+if Should_MAA:
+    Hull = ConvexHull(solutions)
+    
+    plt.figure()
+    
+    DK = solutions[:,0]
+    DE = solutions[:,1]
+    
+    plt.plot(DK, DE,
+             'o', label = "near-optimal")
+    
+    #Plot optimal
+    plt.plot(n.links.p_nom_opt["Island_to_Denmark"], 
+             n.links.p_nom_opt["Island_to_Germany"],
+             '.', markersize = 20, label = "optimum")
+    
+    plt.legend()
+    
+    plt.xlim([-500, n.links.p_nom_max.iloc[0]*1.05])
+    plt.ylim([-500, n.links.p_nom_max.iloc[1]*1.05])
+    
+    for simplex in hull.simplices:
+    
+        plt.plot(solutions[simplex, 0], solutions[simplex, 1], 'k-')
+else:
+    pass
+
---
+++ Pre-project/Case 2/case2_setup.py
@@ -0,0 +1,172 @@
+#%% Import
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep  6 10:44:50 2022
+
+@author: Lukas & Anders
+"""
+
+import pypsa
+import numpy as np
+import matplotlib.pyplot as plt
+import cartopy.crs as ccrs
+import pandas as pd
+import island_lib as il #Library with data and calculation functions 
+import island_plt as ip #Library with plotting functions.
+
+# Load Data price and load data
+cprice, cload = il.get_load_and_price(2030)
+
+# Load wind CF
+cf_wind_df = pd.read_csv(r'Data/Wind/wind_test.csv',index_col = [0], sep=",")
+
+#Load link info
+link_cost_url = 'https://github.com/PyPSA/technology-data/blob/master/inputs/manual_input.csv?raw=true'
+link_cost     = pd.read_csv(link_cost_url)
+link_inv      = link_cost.loc[link_cost['parameter'].str.startswith('investment') & link_cost['technology'].str.startswith('HVDC submarine')]
+link_life     = link_cost.loc[link_cost['parameter'].str.startswith('lifetime') & link_cost['technology'].str.startswith('HVDC submarine')]
+link_FOM      = link_cost.loc[link_cost['parameter'].str.startswith('FOM') & link_cost['technology'].str.startswith('HVDC submarine')]
+#%% Set up network & bus info -------------------------------------
+
+network = pypsa.Network()       #Create network
+t = pd.date_range('2019-01-01 00:00', '2019-12-31 23:00', freq = 'H')
+network.set_snapshots(t)  #Set snapshots of network to the timesteps
+
+#Create dataframe with info on buses. Names, x (Longitude) and y (Latitude) 
+bus_df = pd.DataFrame(
+    np.array([                                #Create numpy array with bus info
+    ["Island",          "EI",   6.68, 56.52], #Energy Island
+    ["Denmark",         "DK",   8.12, 56.37], #Assumed Thorsminde
+    ["Norway",          "NO",   8.02, 58.16], #Assumed Kristiansand
+    ["Germany",         "DE",   8.58, 53.54], #Assumed Bremerhaven
+    ["Netherlands",     "NL",   6.84, 53.44], #Assumed Eemshaven
+    ["Belgium",         "BE",   3.20, 51.32], #Assumed Zeebrugge
+    ["United Kingdom",  "GB",  -1.45, 55.01], #Assumed North Shield
+    ]), 
+    columns = ["Country", "Abbreviation", "X", "Y"]   #Give columns titles
+    )
+
+#%% Add buses -----------------------------------------------------
+#Adding busses from bus_info to network
+for i in range(bus_df.shape[0]):    #i becomes integers
+    network.add(                    #Add component
+        "Bus",                      #Component type
+        bus_df.Country[i],          #Component name
+        x = bus_df.X[i],            #Longitude (for plotting)
+        y = bus_df.Y[i],            #Latitude (for plotting)
+        )
+
+#%% Add links -----------------------------------------------------
+
+#List of link destionations from buses
+link_destinations = network.buses.index.values
+
+for i in link_destinations[1:]:     #i becomes each string in the array
+    network.add(                    #Add component
+        "Link",                     #Component type
+        "Island_to_" + i,           #Component name
+        bus0    = "Island",         #Start Bus
+        bus1    = i,                #End Bus
+        carrier = "DC",             #Define carrier type
+        p_min_pu = -1,              #Make links bi-directional
+        p_nom   = 200,              #Power capacity of link
+        p_nom_extendable = True,    #Extendable links
+        capital_cost = link_inv.loc[link_inv['year'] == 2030].value,         
+        )
+
+#%% Add Generators -------------------------------------------------
+
+#Add wind turbine
+network.add(
+    "Generator",          #Component type
+    "Wind",               #Component name
+    bus = "Island",       #Bus on which component is
+    p_nom = 3000,         #Nominal power [MW]
+    p_max_pu = cf_wind_df['electricity'],         #time-series of power coefficients
+#    p_max_pu = 1,         #time-series of power coefficients
+    carrier = "Wind",
+    marginal_cost = 0.1   #Cost per MW from this source 
+    )
+#%% Add Generators -------------------------------------------------
+
+#Add generators to each country bus with varying marginal costs
+for i in range(1, bus_df.shape[0]):
+    network.add(
+        "Generator",
+        "Gen_" + bus_df.Country[i],
+        bus   = bus_df.Country[i],
+        p_nom = 0, 
+        p_nom_extendable = True,     #Make sure country can always deliver to price
+        capital_cost = 0,            #Same as above
+        marginal_cost = cprice[bus_df.Abbreviation[i]].values,
+        )
+
+
+#%% Add stores
+network.add(
+    "Store",          #Component type
+    "Store1",         #Component name
+    bus = "Island",   #Bus on which component is
+    e_nom = 3000,        #Store capacity
+    e_initial = 0,    #Initially stored energy
+    e_cyclic = True,  #Set store to always end and start at same energy
+    #e_nom_extendable = True, #Allow expansion of capacity
+    #e_nom_max = 3000,
+    )
+
+
+#%% Add Loads ------------------------------------------------------
+
+# Add varying cos loads to each country bus
+for i in range(1, bus_df.shape[0]): #i becomes integers
+    network.add(
+        "Load",
+        "Load_" + bus_df.Country[i],
+        bus   = bus_df.Country[i],
+        p_set = cload[bus_df.Abbreviation[i]].values, 
+        )
+
+network.add(
+    "Load",
+    "Datacenter Load",
+    bus = "Island",
+    p_set = 1000,
+    )
+
+#%% Plotting -------------------------------------------------------
+
+# Geographical map
+plt.rc("figure", figsize=(15, 15))    #Set plot resolution
+
+network.plot(
+    color_geomap = True,              #Coloring on oceans
+    boundaries = [-3, 12, 59, 50.5],  #Boundaries of the plot as [x1,x2,y1,y2]
+    projection=ccrs.EqualEarth()      #Choose cartopy.crs projection
+    )
+
+#%% Solver
+
+#network.lopf(pyomo = False) #Solve dynamic system
+
+ip.makeplots(network) #Plot dynamic results
+
+#%% Plotting
+# plt.plot(figsize = (14,7))
+# plt.figure(dpi=300)         # Set resolution
+
+network.links_t.p0.iloc[:,1].plot(
+    figsize = (14,7),
+    label = "Island to DK",)
+
+network.links_t.p0.iloc[:,2].plot(
+    figsize = (14,7),
+    label = "Island to NO",)
+
+network.links_t.p0.iloc[:,3].plot(
+    figsize = (14,7),
+    label = "Island to DE",)
+
+plt.legend()
+plt.ylabel("Power flow [MW]")
+plt.title("Power flow from Island to countries")
+print('Done')
