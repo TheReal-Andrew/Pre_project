7730b96ab927a03bf8126ee6d9498d867696ae6b
Lukas B. Nordentoft <Lukasbnordentoft@gmail.com>
2022-12-20 21:35:15 +0100

    Case 1 MAA plots

--- Pre-project/Case 1/Case1_results1.py
+++ Pre-project/Case 1/Case1_results1.py
@@ -142,5 +142,51 @@ make_hull(n_1a_MAA_10, n_1a_opt, 0.1, 'MAA_1b_10.eps',
 make_hull(n_1b_MAA_10, n_1b_opt, 0.1, 'MAA_1b_10.eps',
           title = 'MAA Analysis of island without area constraint')
 
+#%% MAA hulls in same plot
+
+def make_hull2(sol1, sol10, n_optimal, name, title, loc = 'upper right',
+              ylim = [None, None], xlim = [None, None]):
+    
+    x1, y1 = sol1[:,0], sol1[:,1]
+    
+    x10, y10 = sol10[:,0], sol10[:,1]
+    
+    fig = plt.figure(figsize = (10,5))
+    
+    colors = ['tab:blue', 'tab:red', 'tab:purple']
+    
+    i = 1
+    for sol in [sol1, sol10]:
+        hull = ConvexHull(sol)
+        for simplex in hull.simplices:
+            plt.plot(sol[simplex, 0], sol[simplex, 1], colors[i])
+        i += 1
+            
+    plt.plot(x1, y1, 'o', label = "Near-optimal, mga = 0.01", color = 'tab:red')
+    plt.plot(x10, y10, 'o', label = "Near-optimal, mga = 0.1", color = 'tab:purple')
+    
+    #Plot optimal
+    plt.plot(n_optimal.generators.p_nom_opt["P2X"], 
+             n_optimal.generators.p_nom_opt["Data"],
+              '.', markersize = 20, label = "Optimal",
+              color = 'tab:blue')
+    plt.xlabel("P2X capacity [MW]")
+    plt.ylabel("Data capacity [MW]")
+    plt.ylim(ylim)
+    plt.xlim(xlim)
+    plt.suptitle(title, fontsize = 22, y = 1)
+    plt.title('Near-feasible space, with mga slack of 0.1 and 0.01', fontsize = 14)
+    plt.legend(loc = loc)
+    
+    fig.savefig(name, format = 'eps', bbox_inches='tight')
+    
+make_hull2(n_1a_MAA, n_1a_MAA_10, n_1a_opt, 'MAA_1a_1_10.eps', 
+           title = 'MAA analysis with area constraint', loc = 'lower center',
+           ylim = [None, 600])
+
+make_hull2(n_1b_MAA, n_1b_MAA_10, n_1b_opt, 'MAA_1b_1_10.eps',
+           title = 'MAA analysis without area constraint', loc = 'lower center',
+           ylim = [None, 600])
+
 #%% Sound
 il.its_britney_bitch(r'C:\Users\lukas\Documents\GitHub\NorthSeaEnergyIsland\Data\Sounds')
